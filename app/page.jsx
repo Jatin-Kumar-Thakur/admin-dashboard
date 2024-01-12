670724817f0d522111164b3777d721e214aeee6f
@Jatin-Kumar-Thakur <63.jatin@gmail.com>
2024-01-12 13:11:36 +0530

    complete project

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,8 @@
 import Link from "next/link"
+import { redirect } from "next/navigation"
 
 const Homepage = () => {
+  redirect('/dashboard');
   return (
     <div className="text-red-800">
       <Link href='/dashboard'>Go to dashboard</Link>
